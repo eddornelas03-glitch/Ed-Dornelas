4210ba68732cd2bcdafab8bb42bbeb012ddf3893
eddornelas03-glitch <202601854623@alunos.estacio.br>
2026-02-19 18:59:32 +0000

    Resolvi conflitos do merge

---
+++ ifelse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int numero1, numero2;
+    numero1 = 15;
+    numero2 = 10;
+    if (numero1 > numero2){
+        printf("numero 1 é maior que numero 2");
+    }
+    else{
+        printf("numero 1 não é maior que numero 2");
+   }
+
+
+}
